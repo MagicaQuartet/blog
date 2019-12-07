ffdd120dbeda06a190e6fc0ab89e979b5983d91b
MagicaQuartet <dev.dskim@gmail.com>
2019-12-07 18:36:33 +0900

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ yarn docs:build
 cd docs/.vuepress/dist
 git init
 git add -A
-git commit -m 'deploy with vuepress'
+git commit -m "deploy with vuepress"
 git push -f https://github.com/MagicaQuartet/MagicaQuartet.github.io.git master
