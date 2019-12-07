49a16142f6db4b7b21385d37f9f62a189af01edd
MagicaQuartet <dev.dskim@gmail.com>
2019-12-07 18:23:37 +0900

    Add post

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,21 @@ module.exports = {
   title: "까막's TIL",
   description: "Dev Note by MagicaQuartet",
   themeConfig: {
-    nav: [{ text: "About", link: "/about/" }]
+    nav: [{ text: "About", link: "/about/" }],
+    sidebar: {
+      "/javascript/": [
+        {
+          title: "Basic",
+          collapsible: true,
+          children: ["/javascript/basic/intro.md"]
+        }
+      ],
+      "/": [
+        {
+          title: "Javascript",
+          path: "/javascript/"
+        }
+      ]
+    }
   }
 };
