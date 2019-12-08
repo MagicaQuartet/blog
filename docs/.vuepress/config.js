f674f89bffbc54827641535f1cff90628836957c
MagicaQuartet <dev.dskim@gmail.com>
2019-12-08 23:29:28 +0900

    Add google analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports = {
   title: "까막's TIL",
   description: "Dev Note by MagicaQuartet",
+  plugins: [
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-154197854-1"
+      }
+    ]
+  ],
   themeConfig: {
     nav: [{ text: "About", link: "/about/" }],
     sidebar: {
